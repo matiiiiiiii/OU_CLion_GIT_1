e31608f7726ba7074833042ff07b12d2ba682ce7
matiiiiiiii <mateusz.fabich@wp.pl>
2022-01-18 18:09:58 +0100

    commit 1

---
+++ cmake-build-debug/CMakeFiles/OU_CLion_GIT_1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OU_CLion_GIT_1.dir/main.c.obj"
+  "OU_CLion_GIT_1.exe"
+  "OU_CLion_GIT_1.exe.manifest"
+  "OU_CLion_GIT_1.pdb"
+  "libOU_CLion_GIT_1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/OU_CLion_GIT_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
