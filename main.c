a8f5ccc8f88d551a349bebbcd1c5e1c0a16532df
matiiiiiiii <mateusz.fabich@wp.pl>
2022-01-18 18:18:13 +0100

    pierwsza operacja

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
+int Policz (int a,int b)
+{
+    return a+b;
+}
 
 int main( )
 {
     printf("Hello, World!\n");
+    printf("W pierwszym branchu %d", Policz(2,3));
     return 0;
 }
